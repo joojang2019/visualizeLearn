5f1ba62f5d053dd73d8cc9dc9907d8234ab74775
JooYoung Jang <joojang2019@u.northwestern.edu>
2020-02-17 09:05:19 -0600

    displayed one simple graph

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import BarChart from "./componenets/BarChart";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <p>New app</p>
+      <BarChart />
     </div>
   );
 }
